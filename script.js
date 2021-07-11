2ced0b226c9fab4931d157b73cd82456a027d0dc
Mohamed <mohamed.h.abukar@gmail.com>
2021-07-11 13:08:09 -0500

    updated file.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var userSearch = localStorage.getItem("search")
 var apiKey = "5d1b3f150d64e1e1cd5a4bedbf160469"
 
 function storeInput(cityName) {
-    var apiUrlcitySearch = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    var apiUrlcitySearch = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
     fetch(apiUrlcitySearch)
         .then(function (response) {
             return response.json()
